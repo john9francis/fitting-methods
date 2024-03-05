5f441d728d43427cf1fcf4c922b66832135deec8
John Francis <john9francis@gmail.com>
2024-03-04 17:00:44 -0700

    added a chi-squared function but for now it is returning extremely large values

--- grid_search.py
+++ grid_search.py
@@ -21,10 +21,43 @@ class GridSearch():
       13, 42, 90, 75, 29, 13, 8, 4, 6, 3
     ])
 
+    self.desired_chi_2 = 1
+    self.error_chi_2 = .5
 
-  def fit_function(self):
-    return 
+
+  def fit_function(self, xi):
+
+    def L(mu, gamma):
+      '''Lorentzian distribution'''
+      return (gamma/2) / (np.pi * ((xi - mu) ** 2 + (gamma / 2) ** 2))
+    
+    return self.a1 * L(self.mu1, self.gamma1) + self.a2 * L(self.mu1, self.gamma1)
   
-  def calc_chi_squared(self):
-    return 
+  def calc_chi_squared(self) -> float:
+    '''
+    Returns a value of the chi squared test
+    based on the y data and the fit function
+    '''
+    chi_squared = 0
 
+    for i in range(len(self.x_data)):
+
+      yi = self.y_data[i]
+      xi = self.x_data[i]
+
+      y_func_i = self.fit_function(xi)
+      # assuming uncertainty in y is squareroot of yi
+      # then sigma squared is just yi
+      chi_squared += (yi - y_func_i)**2 / yi
+
+    return chi_squared
+
+
+
+
+
+if __name__ == "__main__":
+  grid_search = GridSearch()
+  chi_2 = grid_search.calc_chi_squared()
+
+  print(chi_2)
