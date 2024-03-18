9abe59def09ba8bc2db185079679aaa13ec37030
John Francis <john9francis@gmail.com>
2024-03-18 16:37:27 -0600

    Created a creature list class

---
+++ genetic_algorithm/creature_list.py
@@ -0,0 +1,6 @@
+# A list to take care of mutating, deleting, etc. creatures.
+
+class CreatureList:
+  def __init__(self, how_many_creatures:int) -> None:
+    self.creature_amount = how_many_creatures
+    pass
