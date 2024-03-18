a867debb599b1c632658bc624b172360fc4f1ac4
John Francis <john9francis@gmail.com>
2024-03-18 16:34:54 -0600

    Created creatures that randomly generate parameters. Next step is to start evolving them.

--- genetic_algorithm/creature.py
+++ genetic_algorithm/creature.py
@@ -1,16 +1,127 @@
 import random
+import copy
 import numpy as np
+from matplotlib import pyplot as plt
 
 class Creature:
-  def __init__(self) -> None:
+  def __init__(self, param_list:list = []) -> None:
 
     self.rand = random.Random()
 
-    pass
+    # parameters
+    # (a - g) is of length 7
+    self.param_list = [0, 0, 0, 0, 0, 0, 0]
+
+    if len(param_list) == 0:
+      self.set_random_params(0, 100)
+    else:
+      self.param_list = param_list
 
 
-  def fit_function(self):
+    self.x_data = np.array([])
+    self.y_data = np.array([])
+    self.y_uncertainties = np.array([])
+    # Assuming the x_uncertainties are much less than y_uncertainties.
+    self.y_fit = np.array([])
+
+    self.chi_sq = 0
+
+
+  def set_random_params(self, min, max):
+    for i in range(len(self.param_list)):
+      self.param_list[i] = self.rand.uniform(min, max)
+
+
+  def set_params(self, param_list):
     '''
-    The fit function is a gamma multiplied by an arctan.
+    Takes in array:
+    [a,b,c,d,e,f,g]
+    and sets this creature's params to them
     '''
-    return np.pi
+    for i in range(len(self.param_list)):
+      self.param_list[i] = param_list[i]
+
+
+  def get_params(self):
+    '''
+    returns a copy of the parameters
+    '''
+    return copy.deepcopy(self.param_list)
+  
+
+
+  def mutate(self, amount:float):
+    '''
+    Takes in an amount for how much to mutate a param, then
+    changes one random param by either +,-,*,/
+    '''
+    indx_to_change = self.rand.randint(0, len(self.param_list) - 1)
+    param_to_change = self.param_list[indx_to_change]
+
+    random_val = self.rand.uniform(0,1)
+    t_or_f = self.rand.randint(0, 1)
+
+    if random_val > .9:
+      if t_or_f == 0:
+        param_to_change *= amount
+      else:
+        param_to_change /= amount
+    elif random_val > .5:
+      if t_or_f == 0:
+        param_to_change += amount
+      else:
+        param_to_change -= amount
+      
+    self.param_list[indx_to_change] = param_to_change
+
+
+
+
+  def set_data(self, y_data:np.ndarray, x_data:np.ndarray = np.array([])):
+    if x_data.size == 0:
+      x_data = np.arange(0, len(y_data), 1.)
+    self.x_data = x_data
+    self.y_data = y_data
+
+    self.y_uncertainties = np.sqrt(y_data)
+    self.y_fit = self.fit_function(x_data)
+
+    self.chi_sq = self.calculate_chi_squared()
+
+
+  def fit_function(self, x:np.ndarray) -> np.ndarray:
+    '''
+    Takes in an array of x_values
+    The fit function is a gamma added to an arctan:
+    f(x) = a * x^(b-1)*exp(-x/c) + d*arctan(e*x+f) + g
+    Returns an array of the fit values
+    '''
+    a = self.param_list[0]
+    b = self.param_list[1]
+    c = self.param_list[2]
+    d = self.param_list[3]
+    e = self.param_list[4]
+    f = self.param_list[5]
+    g = self.param_list[6]
+    fit = a * x ** (b - 1) * np.exp( -x / c) + d * np.arctan(e * x + f) + g
+    
+    return fit
+  
+
+  def calculate_chi_squared(self):
+    chi_sq = 0.
+    for i in range(len(self.x_data)):
+      chi_sq += (self.y_data[i] - self.y_fit[i]) ** 2 / (self.y_uncertainties[i] ** 2)
+
+    return chi_sq
+  
+
+  def plot_fit(self):
+    plt.plot(self.x_data, self.y_data, label="original data")
+    plt.plot(self.x_data, self.y_fit, label="fit")
+    plt.title("Curve fit")
+    plt.legend()
+
+    print(f"The chi-squared value of this fit is: {self.chi_sq}.")
+
+    plt.show()
--- genetic_algorithm/main.py
+++ genetic_algorithm/main.py
@@ -1,10 +1,15 @@
 from creature import Creature
+import numpy as np
+from matplotlib import pyplot as plt
 
 def main():
+  # get data
+  data = [.1,1.,2.,3.,2.,5.,2.,2.,2.,1.,.1]
+
+  # get a creature and test the fit
   c = Creature()
-  print(c.fit_function())
-
-
+  c.set_data(data)
+  c.plot_fit()
 
 
 if __name__ == "__main__":
